b6fdab559db8fb19d9be8b35232181e283c84809
Siyanda Mvunyiswa <steeroy10@gmail.com>
2022-12-16 01:06:49 +0200

    Appointment Card

---
+++ index.js
@@ -0,0 +1,35 @@
+const slider = document.querySelectorAll('.content__wrapper');
+
+var indexSlide = 0;
+
+slider.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slideShow = () => {
+  slider.forEach((slide) => {
+    slide.style.transform = `translateX(-${indexSlide * 100}%)`;
+  });
+};
+
+const previousPatient = () => {
+  indexSlide--;
+  slideShow();
+};
+
+const nextPatient = () => {
+  indexSlide++;
+  slideShow();
+};
+
+var show;
+
+const menuOpen = () => {
+  if (show == 1) {
+    document.getElementById('menus').style.display = 'block';
+    return (show = 0);
+  } else {
+    document.getElementById('menus').style.display = 'none';
+    return (show = 1);
+  }
+};
